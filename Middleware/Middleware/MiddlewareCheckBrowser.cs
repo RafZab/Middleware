0d87c51550032a30c8c12593f197df37bee594ac
Rafał Zaborowski <raffik2000@gmail.com>
2021-04-20 16:31:27 +0200

    Addition Middleware

---
+++ Middleware/Middleware/MiddlewareCheckBrowser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware.Middleware
+{
+    public class MiddlewareCheckBrowser : IMiddleware
+    {
+        private readonly IBrowserDetector _browserDetector;
+        public MiddlewareCheckBrowser(IBrowserDetector browserDetector)
+        {
+            _browserDetector = browserDetector;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var browser = _browserDetector.Browser;
+            if (!ChcekTheBrowserSupported(browser.Name))
+            {
+                await context.Response.WriteAsync("Browser not supported");
+            }
+            await next.Invoke(context);
+        }
+
+        /// <summary>
+        /// Check that the browser is supported.
+        /// </summary>
+        /// <param name="browser"></param>
+        /// <returns> If browser is supported return true if not return false. </returns>
+        private bool ChcekTheBrowserSupported(string browser)
+        {
+            if (browser == "InternetExplorer" || browser == "Edge" || browser == "EdgeChromium")
+                return false;
+            return true;
+        }
+    }
+}
